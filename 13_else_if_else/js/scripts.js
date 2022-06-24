b2285c208e1777978efff24a58647d177d47ecda
Átila Cassel <atilacassel@MacBook-Pro-de-Atila.local>
2022-06-24 17:21:51 -0300

    aulas 13 e 14

--- 13_else_if_else/js/scripts.js
+++ 13_else_if_else/js/scripts.js
@@ -1,7 +1,9 @@
-var nome = "Átila";
+var nome = "Matheus";
 
 if(nome == "Pedro") {
     console.log("O nome dele é Pedro");
+} else if(nome == "Matheus") {
+    console.log("O nome dele é Matheus")
 } else {
     console.log("O nome dele é " + nome);
 }
---
+++ 14_tipo_valor/js/scripts.js
@@ -0,0 +1,8 @@
+var numero = '5';
+
+if(numero === 5) {
+    console.log("O numero é 5 do tipo number");
+} else if(numero !== 5) {
+    console.log("Não é numero 5 do tipo number")
+}
+
