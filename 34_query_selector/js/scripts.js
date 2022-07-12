3b880db4d2f163643a587100633b98da5d1f35d6
Átila Cassel <atilacassel@MacBook-Pro-de-Atila.local>
2022-07-12 13:46:42 -0300

    aulas 33 até 36

---
+++ 34_query_selector/js/scripts.js
@@ -0,0 +1,19 @@
+var itensClass = document.getElementsByClassName('item');
+console.log(itensClass);
+
+// querySelectorAll
+var itensQuery = document.querySelectorAll('#lista2 li');
+console.log(itensQuery);
+
+var itensQuery2 = document.querySelectorAll('#lista .item');
+console.log(itensQuery2);
+
+// querySelector
+var lista = document.querySelector('#lista');
+console.log(lista);
+
+var primeiraLista = document.querySelector('ul');
+console.log(primeiraLista);
+
+var span = document.querySelectorAll('#paragrafo span');
+console.log(span);
